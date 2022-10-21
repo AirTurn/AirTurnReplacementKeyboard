837c3a2c47b9a86b79726bee492735a468f13423
Nick Brook <nrbrook@gmail.com>
2022-10-21 18:14:27 +0100

    Replace globe button with one that works

--- Sources/AirTurnReplacementKeyboard/KeyboardAppearance.swift
+++ Sources/AirTurnReplacementKeyboard/KeyboardAppearance.swift
@@ -18,6 +18,9 @@ class KeyboardAppearance: StandardKeyboardAppearance {
         if action == .custom(named: "dismiss") {
             return super.buttonImage(for: .dismissKeyboard)
         }
+        if action == .nextLocale {
+            return super.buttonImage(for: .nextKeyboard)
+        }
         return super.buttonImage(for: action)
     }
     
@@ -27,12 +30,18 @@ class KeyboardAppearance: StandardKeyboardAppearance {
         if action == .custom(named: "dismiss") {
             return super.buttonStyle(for: .dismissKeyboard, isPressed: false)
         }
+        if action == .nextLocale {
+            return super.buttonStyle(for: .nextKeyboard, isPressed: isPressed)
+        }
         return super.buttonStyle(for: action, isPressed: isPressed)
     }
     
     override func buttonText(for action: KeyboardAction) -> String? {
         if action == .custom(named: "dismiss") {
             return super.buttonText(for: .dismissKeyboard)
+        }
+        if action == .nextLocale {
+            return super.buttonText(for: .nextKeyboard)
         }
         return super.buttonText(for: action)
     }
--- Sources/AirTurnReplacementKeyboard/KeyboardLayoutProvider.swift
+++ Sources/AirTurnReplacementKeyboard/KeyboardLayoutProvider.swift
@@ -20,8 +20,9 @@ class KeyboardLayoutProvider: StandardKeyboardLayoutProvider {
         guard rows.count > 0, context.locales.count > 1 else { return layout }
         let rowIndex = rows.count - 1
         guard let system = (rows[rowIndex].first { $0.action.isSystemAction }) else { return layout }
-        let switcher = KeyboardLayoutItem(action: .nextLocale, size: system.size, insets: system.insets)
-        rows.insert(switcher, after: .space, atRow: rowIndex)
+
+        let newGlobe = KeyboardLayoutItem(action: .nextLocale, size: system.size, insets: system.insets)
+        rows.insert(newGlobe, before: .space, atRow: rowIndex)
 
         let dismiss = (rows[rowIndex].first { $0.action == .dismissKeyboard })
         if dismiss != nil {
